36f1f90913bbf57b35b7a9973abffa4ba19c7a5f
user <ladasergeevna@mail.ru>
2024-10-25 17:06:59 +0900

    All tasks done

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,73 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        //Task 1
+        System.out.println("Task 1");
+        int [] array1 = new int [5];
+        array1[0] = 2500;
+        array1[1] = 10000;
+        array1[2] = 12347;
+        array1[3] = 15000;
+        array1[4] = 1000;
+        int sum = 0;
+        for (int i = 0; i< array1.length; i++) {
+            sum = sum + array1[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Task 2
+        System.out.println();
+        System.out.println("Task 2");
+        int maxSum = array1[0];
+        int minSum = array1[0];
+        for (int i = 0; i< array1.length; i++) {
+            final int current = array1[i];
+            if (current > maxSum) {
+                maxSum = current;
+            }
+            if (current < minSum) {
+                minSum = current;
+            }
+        }
+        System.out.println("Миниимальная сумма трат за неделю составила " + minSum + " рублей. Максимальная сумма трат за неделю составила " + maxSum + " рублей");
+
+        //Task 2_1. for each
+        System.out.println();
+        System.out.println("Task 2_1");
+        for (final int current : array1) {
+            if (current > maxSum) {
+                maxSum = current;
+            }
+            if (current < minSum) {
+                minSum = current;
+            }
+        }
+        System.out.println("Миниимальная сумма трат за неделю составила " + minSum + " рублей. Максимальная сумма трат за неделю составила " + maxSum + " рублей");
+
+        //Task 3
+        System.out.println();
+        System.out.println("Task 3");
+        double [] array2 = new double [5];
+        array2[0] = 2500;
+        array2[1] = 10000;
+        array2[2] = 12347;
+        array2[3] = 15000;
+        array2[4] = 1000;
+
+        double averageSum = 0;
+        double sum2 = 0F;
+        for (int i = 0; i < array2.length; i++) {
+            sum2 = sum2 + array2[i];
+        }
+        averageSum = sum2 / array2.length;
+        System.out.println("Средняя сумма трат за месяц составила " + averageSum + " рублей.");
+
+        //Task 4
+        System.out.println();
+        System.out.println("Task 4");
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >=0 ; i--) {
+                System.out.print(reverseFullName[i]);
         }
     }
 }
